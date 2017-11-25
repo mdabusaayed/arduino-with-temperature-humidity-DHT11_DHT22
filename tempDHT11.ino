c3c38c4fe1d465df312890a287d144b68556b812
Mohammad Abu Sayed <mdabusaayed@gmail.com>
2017-11-25 19:54:27 +0600

    Add files via upload

---
+++ tempDHT11.ino
@@ -0,0 +1,22 @@
+#include <dht.h>
+
+#define dht_dpin A0
+
+dht DHT;
+
+void setup(){
+  Serial.begin(9600);
+  delay(700);
+  
+}
+void loop(){
+  DHT.read11(dht_dpin);
+
+    Serial.print("\n Air Humidity= ");
+    Serial.print(DHT.humidity);
+    Serial.print("%  ");
+    Serial.print("Temp= ");
+    Serial.print(DHT.temperature); 
+    Serial.println("°C  ");
+  delay(3000);
+}
